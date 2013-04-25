079cda6177ad080834057b4848f49a550d74c289
costache.theodor <costache.theodor@322df269-9f73-6550-525d-11b85d1d7789>
2013-04-25 10:56:57 +0000

    Issue 13

--- JavaCalendar/src/main/java/de/costache/calendar/ui/strategy/Config.java
+++ JavaCalendar/src/main/java/de/costache/calendar/ui/strategy/Config.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Theodor Costache
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,209 +15,174 @@
  */
 package de.costache.calendar.ui.strategy;
 
-import java.awt.Color;
+import de.costache.calendar.model.CalendarEvent;
+import de.costache.calendar.util.CalendarUtil;
+
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-import de.costache.calendar.util.CalendarUtil;
-
 /**
- * 
  * @author theodorcostache
- * 
  */
 public class Config {
 
-	private Color lineColor;
-
-	private Color middleLineColor;
-
-	private Color dayHeaderForegroundColor;
-
-	private Color dayHeaderBackgroundColor;
-
-	private Color todayHeaderForegroundColor;
-
-	private Color todayHeaderBackgroundColor;
-
-	private Color eventDefaultBackgroundColor;
-
-	private Color eventDefaultForegroundColor;
-
-	private int workingHoursStart;
-
-	private int workingHoursEnd;
-
-	private Color outsideWorkingHoursColor;
-
-	private Color dayDisabledBackgroundColor;
-
-	private boolean allDayPanelVisible;
-
-	private List<Date> holidays;
-
-	private Color holidayBgColor;
-
+    private Color lineColor;
+    private Color middleLineColor;
+    private Color dayHeaderForegroundColor;
+    private Color dayHeaderBackgroundColor;
+    private Color todayHeaderForegroundColor;
+    private Color todayHeaderBackgroundColor;
+    private Color eventDefaultBackgroundColor;
+    private Color eventDefaultForegroundColor;
+    private int workingHoursStart;
+    private int workingHoursEnd;
+    private Color outsideWorkingHoursColor;
+    private Color dayDisabledBackgroundColor;
+    private boolean allDayPanelVisible;
+    private Color holidayBgColor;
     private Calendar intervalStart;
-
     private Calendar intervalEnd;
 
-	public Config() {
-		lineColor = new Color(220, 220, 220);
-		middleLineColor = new Color(240, 240, 240);
+    public Config() {
+        lineColor = new Color(220, 220, 220);
+        middleLineColor = new Color(240, 240, 240);
 
-		todayHeaderBackgroundColor = new Color(240, 230, 140, 128);
-		dayHeaderBackgroundColor = new Color(173, 216, 230, 200);
+        todayHeaderBackgroundColor = new Color(240, 230, 140, 128);
+        dayHeaderBackgroundColor = new Color(173, 216, 230, 200);
 
-		todayHeaderForegroundColor = Color.black;
-		dayHeaderForegroundColor = Color.black;
+        todayHeaderForegroundColor = Color.black;
+        dayHeaderForegroundColor = Color.black;
 
-		eventDefaultBackgroundColor = new Color(135, 184, 217, 128);
-		eventDefaultForegroundColor = Color.DARK_GRAY;
+        eventDefaultBackgroundColor = new Color(135, 184, 217, 128);
+        eventDefaultForegroundColor = Color.DARK_GRAY;
 
-		workingHoursStart = 8;
-		workingHoursEnd = 17;
+        workingHoursStart = 8;
+        workingHoursEnd = 17;
 
-		outsideWorkingHoursColor = new Color(148, 197, 217, 40);
-		dayDisabledBackgroundColor = new Color(148, 197, 217, 128);
+        outsideWorkingHoursColor = new Color(148, 197, 217, 40);
+        dayDisabledBackgroundColor = new Color(148, 197, 217, 128);
 
-		allDayPanelVisible = true;
+        allDayPanelVisible = true;
 
-		holidayBgColor = new Color(220,220,220);
-		holidays = new ArrayList<Date>();
-	}
+        holidayBgColor = new Color(220, 220, 220);
+    }
 
-	public Color getLineColor() {
-		return lineColor;
-	}
+    public Color getLineColor() {
+        return lineColor;
+    }
 
-	public void setLineColor(Color lineColor) {
-		this.lineColor = lineColor;
-	}
+    public void setLineColor(Color lineColor) {
+        this.lineColor = lineColor;
+    }
 
-	public Color getMiddleLineColor() {
-		return middleLineColor;
-	}
+    public Color getMiddleLineColor() {
+        return middleLineColor;
+    }
 
-	public void setMiddleLineColor(Color middleLineColor) {
-		this.middleLineColor = middleLineColor;
-	}
+    public void setMiddleLineColor(Color middleLineColor) {
+        this.middleLineColor = middleLineColor;
+    }
 
-	public Color getDayHeaderBackgroundColor() {
-		return dayHeaderBackgroundColor;
-	}
+    public Color getDayHeaderBackgroundColor() {
+        return dayHeaderBackgroundColor;
+    }
 
-	public void setDayHeaderBackgroundColor(Color dayHeaderBackgroundColor) {
-		this.dayHeaderBackgroundColor = dayHeaderBackgroundColor;
-	}
+    public void setDayHeaderBackgroundColor(Color dayHeaderBackgroundColor) {
+        this.dayHeaderBackgroundColor = dayHeaderBackgroundColor;
+    }
 
-	public Color getDayHeaderForegroundColor() {
-		return dayHeaderForegroundColor;
-	}
+    public Color getDayHeaderForegroundColor() {
+        return dayHeaderForegroundColor;
+    }
 
-	public void setDayHeaderForegroundColor(Color dayHeaderForegroundColor) {
-		this.dayHeaderForegroundColor = dayHeaderForegroundColor;
-	}
+    public void setDayHeaderForegroundColor(Color dayHeaderForegroundColor) {
+        this.dayHeaderForegroundColor = dayHeaderForegroundColor;
+    }
 
-	public Color getTodayHeaderBackgroundColor() {
-		return todayHeaderBackgroundColor;
-	}
+    public Color getTodayHeaderBackgroundColor() {
+        return todayHeaderBackgroundColor;
+    }
 
-	public void setTodayHeaderBackgroundColor(Color todayHeaderBackgroundColor) {
-		this.todayHeaderBackgroundColor = todayHeaderBackgroundColor;
-	}
+    public void setTodayHeaderBackgroundColor(Color todayHeaderBackgroundColor) {
+        this.todayHeaderBackgroundColor = todayHeaderBackgroundColor;
+    }
 
-	public Color getTodayHeaderForegroundColor() {
-		return todayHeaderForegroundColor;
-	}
+    public Color getTodayHeaderForegroundColor() {
+        return todayHeaderForegroundColor;
+    }
 
-	public void setTodayHeaderForegroundColor(Color todayHeaderForegroundColor) {
-		this.todayHeaderForegroundColor = todayHeaderForegroundColor;
-	}
+    public void setTodayHeaderForegroundColor(Color todayHeaderForegroundColor) {
+        this.todayHeaderForegroundColor = todayHeaderForegroundColor;
+    }
 
-	public Color getEventDefaultBackgroundColor() {
-		return eventDefaultBackgroundColor;
-	}
+    public Color getEventDefaultBackgroundColor() {
+        return eventDefaultBackgroundColor;
+    }
 
-	public void setEventDefaultBackgroundColor(Color eventDefaultBackgroundColor) {
-		this.eventDefaultBackgroundColor = eventDefaultBackgroundColor;
-	}
+    public void setEventDefaultBackgroundColor(Color eventDefaultBackgroundColor) {
+        this.eventDefaultBackgroundColor = eventDefaultBackgroundColor;
+    }
 
-	public Color getEventDefaultForegroundColor() {
-		return eventDefaultForegroundColor;
-	}
+    public Color getEventDefaultForegroundColor() {
+        return eventDefaultForegroundColor;
+    }
 
-	public void setEventDefaultForegroundColor(Color eventDefaultForegroundColor) {
-		this.eventDefaultForegroundColor = eventDefaultForegroundColor;
-	}
+    public void setEventDefaultForegroundColor(Color eventDefaultForegroundColor) {
+        this.eventDefaultForegroundColor = eventDefaultForegroundColor;
+    }
 
-	public int getWorkingHoursEnd() {
-		return workingHoursEnd;
-	}
+    public int getWorkingHoursEnd() {
+        return workingHoursEnd;
+    }
 
-	public void setWorkingHoursEnd(int workingHoursEnd) {
-		this.workingHoursEnd = workingHoursEnd;
-	}
+    public void setWorkingHoursEnd(int workingHoursEnd) {
+        this.workingHoursEnd = workingHoursEnd;
+    }
 
-	public int getWorkingHoursStart() {
-		return workingHoursStart;
-	}
+    public int getWorkingHoursStart() {
+        return workingHoursStart;
+    }
 
-	public void setWorkingHoursStart(int workingHoursStart) {
-		this.workingHoursStart = workingHoursStart;
-	}
+    public void setWorkingHoursStart(int workingHoursStart) {
+        this.workingHoursStart = workingHoursStart;
+    }
 
-	public Color getOutsideWorkingHoursColor() {
-		return outsideWorkingHoursColor;
-	}
+    public Color getOutsideWorkingHoursColor() {
+        return outsideWorkingHoursColor;
+    }
 
-	public void setOutsideWorkingHoursColor(Color outsideWorkingHoursColor) {
-		this.outsideWorkingHoursColor = outsideWorkingHoursColor;
-	}
+    public void setOutsideWorkingHoursColor(Color outsideWorkingHoursColor) {
+        this.outsideWorkingHoursColor = outsideWorkingHoursColor;
+    }
 
-	public Color getDayDisabledBackgroundColor() {
-		return dayDisabledBackgroundColor;
-	}
+    public Color getDayDisabledBackgroundColor() {
+        return dayDisabledBackgroundColor;
+    }
 
-	public void setDayDisabledBackgroundColor(Color dayDisabledBackgroundColor) {
-		this.dayDisabledBackgroundColor = dayDisabledBackgroundColor;
-	}
+    public void setDayDisabledBackgroundColor(Color dayDisabledBackgroundColor) {
+        this.dayDisabledBackgroundColor = dayDisabledBackgroundColor;
+    }
 
-	public boolean isAllDayPanelVisible() {
-		return allDayPanelVisible;
-	}
+    public boolean isAllDayPanelVisible() {
+        return allDayPanelVisible;
+    }
 
-	public void setAllDayPanelVisible(boolean allDayPanelVisible) {
-		this.allDayPanelVisible = allDayPanelVisible;
-	}
+    public void setAllDayPanelVisible(boolean allDayPanelVisible) {
+        this.allDayPanelVisible = allDayPanelVisible;
+    }
 
-	public List<Date> getHolidays() {
-		return holidays;
-	}
+    public Color getHolidayBgColor() {
+        return holidayBgColor;
+    }
 
-	public void setHolidays(List<Date> holidays) {
-		this.holidays.clear();
-		for (Date date : holidays) {
+    public void setHolidayBgColor(Color holidayBgColor) {
+        this.holidayBgColor = holidayBgColor;
+    }
 
-			this.holidays.add(CalendarUtil.stripTime(date));
-		}
-	}
-
-	public boolean isHoliday(Date date) {
-		return holidays.contains(CalendarUtil.stripTime(date));
-	}
-
-	public Color getHolidayBgColor() {
-		return holidayBgColor;
-	}
-
-	public void setHolidayBgColor(Color holidayBgColor) {
-		this.holidayBgColor = holidayBgColor;
-	}
-
-    public Calendar getIntervalStart(){
+    public Calendar getIntervalStart() {
         return intervalStart;
     }
 
--- JavaCalendar/src/main/java/de/costache/calendar/util/IndexedEventCollection.java
+++ JavaCalendar/src/main/java/de/costache/calendar/util/IndexedEventCollection.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Theodor Costache
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,191 +15,221 @@
  */
 package de.costache.calendar.util;
 
-import java.util.*;
-
-import org.apache.commons.collections.MultiHashMap;
-
 import de.costache.calendar.JCalendar;
-import de.costache.calendar.events.*;
+import de.costache.calendar.events.ModelChangedEvent;
+import de.costache.calendar.events.ModelChangedListener;
+import de.costache.calendar.events.SelectionChangedEvent;
+import de.costache.calendar.events.SelectionChangedListener;
 import de.costache.calendar.model.CalendarEvent;
 import de.costache.calendar.model.CalendarEvent.Property;
+import org.apache.commons.collections.MultiHashMap;
+
+import java.util.*;
 
 /**
- * 
  * @author theodorcostache
- * 
  */
 class IndexedEventCollection extends Observable implements Observer, EventCollection {
 
-	private final MultiHashMap indexedEvents;
+    private final MultiHashMap indexedEvents;
+    private final List<ModelChangedListener> collectionChangedListeners;
+    private final List<SelectionChangedListener> selectionChangedListeners;
+    private final Set<CalendarEvent> selectedEvents;
+    private final JCalendar parent;
 
-	private final List<ModelChangedListener> collectionChangedListeners;
+    /**
+     * Creates a new instance of {@link IndexedEventCollection}
+     */
+    public IndexedEventCollection(final JCalendar parent) {
+        this.parent = parent;
+        this.indexedEvents = new MultiHashMap();
+        this.collectionChangedListeners = new ArrayList<ModelChangedListener>();
+        this.selectionChangedListeners = new ArrayList<SelectionChangedListener>();
+        this.selectedEvents = new HashSet<CalendarEvent>();
+    }
 
-	private final List<SelectionChangedListener> selectionChangedListeners;
+    @Override
+    public void add(final CalendarEvent calendarEvent) {
+        calendarEvent.addObserver(this);
+        final Collection<Date> dates = CalendarUtil.getDates(calendarEvent.getStart(), calendarEvent.getEnd());
+        for (final Date date : dates) {
 
-	private final Set<CalendarEvent> selectedEvents;
+            if (!indexedEvents.containsValue(calendarEvent)) {
+                indexedEvents.put(date, calendarEvent);
+            }
+        }
+        notifyObservers();
 
-	private final JCalendar parent;
+        final ModelChangedEvent event = new ModelChangedEvent(parent, calendarEvent);
+        for (final ModelChangedListener listener : collectionChangedListeners) {
+            listener.eventAdded(event);
+        }
+    }
 
-	/**
-	 * Creates a new instance of {@link IndexedEventCollection}
-	 */
-	public IndexedEventCollection(final JCalendar parent) {
-		this.parent = parent;
-		this.indexedEvents = new MultiHashMap();
-		this.collectionChangedListeners = new ArrayList<ModelChangedListener>();
-		this.selectionChangedListeners = new ArrayList<SelectionChangedListener>();
-		this.selectedEvents = new HashSet<CalendarEvent>();
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public void remove(final CalendarEvent calendarEvent) {
+        calendarEvent.deleteObserver(this);
+        selectedEvents.remove(calendarEvent);
 
-	@Override
-	public void add(final CalendarEvent calendarEvent) {
-		calendarEvent.addObserver(this);
-		final Collection<Date> dates = CalendarUtil.getDates(calendarEvent.getStart(), calendarEvent.getEnd());
-		for (final Date date : dates) {
+        for (final Object key : new HashSet<Object>(indexedEvents.keySet())) {
+            final Collection<CalendarEvent> events = indexedEvents.getCollection(key);
+            if (events.contains(calendarEvent))
+                indexedEvents.remove(key, calendarEvent);
+        }
 
-			if (!indexedEvents.containsValue(calendarEvent)) {
-				indexedEvents.put(date, calendarEvent);
-			}
-		}
-		notifyObservers();
+        notifyObservers();
 
-		final ModelChangedEvent event = new ModelChangedEvent(parent, calendarEvent);
-		for (final ModelChangedListener listener : collectionChangedListeners) {
-			listener.eventAdded(event);
-		}
-	}
+        final ModelChangedEvent event = new ModelChangedEvent(parent, calendarEvent);
+        for (final ModelChangedListener listener : collectionChangedListeners) {
+            listener.eventRemoved(event);
+        }
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public void remove(final CalendarEvent calendarEvent) {
-		calendarEvent.deleteObserver(this);
-		selectedEvents.remove(calendarEvent);
+    @Override
+    @SuppressWarnings("unchecked")
+    public void removeAll(final List<CalendarEvent> calendarEvents) {
+        for (CalendarEvent calendarEvent : calendarEvents) {
+            remove(calendarEvent);
+        }
+    }
 
-		for (final Object key : new HashSet<Object>(indexedEvents.keySet())) {
-			final Collection<CalendarEvent> events = indexedEvents.getCollection(key);
-			if (events.contains(calendarEvent))
-				indexedEvents.remove(key, calendarEvent);
-		}
+    @Override
+    public void addSelected(final CalendarEvent calendarEvent) {
+        selectedEvents.add(calendarEvent);
+        final SelectionChangedEvent event = new SelectionChangedEvent(calendarEvent);
+        for (final SelectionChangedListener listener : selectionChangedListeners) {
+            listener.selectionChanged(event);
+        }
+    }
 
-		notifyObservers();
+    @Override
+    public void removeSelected(final CalendarEvent calendarEvent) {
+        final boolean remove = selectedEvents.remove(calendarEvent);
+        if (remove) {
+            final SelectionChangedEvent event = new SelectionChangedEvent(calendarEvent);
+            for (final SelectionChangedListener listener : selectionChangedListeners) {
+                listener.selectionChanged(event);
+            }
+        }
 
-		final ModelChangedEvent event = new ModelChangedEvent(parent, calendarEvent);
-		for (final ModelChangedListener listener : collectionChangedListeners) {
-			listener.eventRemoved(event);
-		}
-	}
+    }
 
-	@Override
-	public void addSelected(final CalendarEvent calendarEvent) {
-		selectedEvents.add(calendarEvent);
-		final SelectionChangedEvent event = new SelectionChangedEvent(calendarEvent);
-		for (final SelectionChangedListener listener : selectionChangedListeners) {
-			listener.selectionChanged(event);
-		}
-	}
+    @Override
+    public void clearSelected(final CalendarEvent toIgnore, final boolean notifyListeners) {
+        for (final CalendarEvent event : selectedEvents) {
+            if (event != toIgnore) {
+                event.setSelected(false);
+            }
+        }
+        selectedEvents.clear();
 
-	@Override
-	public void removeSelected(final CalendarEvent calendarEvent) {
-		final boolean remove = selectedEvents.remove(calendarEvent);
-		if (remove) {
-			final SelectionChangedEvent event = new SelectionChangedEvent(calendarEvent);
-			for (final SelectionChangedListener listener : selectionChangedListeners) {
-				listener.selectionChanged(event);
-			}
-		}
+        if (notifyListeners) {
+            final SelectionChangedEvent event = new SelectionChangedEvent(null);
+            for (final SelectionChangedListener listener : selectionChangedListeners) {
+                listener.selectionChanged(event);
+            }
+        }
+    }
 
-	}
+    @Override
+    public Collection<CalendarEvent> getSelectedEvents() {
+        return Collections.unmodifiableSet(new HashSet<CalendarEvent>(selectedEvents));
+    }
 
-	@Override
-	public void clearSelected(final CalendarEvent toIgnore, final boolean notifyListeners) {
-		for (final CalendarEvent event : selectedEvents) {
-			if (event != toIgnore) {
-				event.setSelected(false);
-			}
-		}
-		selectedEvents.clear();
+    @Override
+    public Collection<CalendarEvent> getEvents(final Date date) {
+        @SuppressWarnings("rawtypes")
+        final Collection events = indexedEvents.getCollection(CalendarUtil.stripTime(date));
+        if (events == null)
+            return new ArrayList<CalendarEvent>();
+        @SuppressWarnings("unchecked")
+        final List<CalendarEvent> result = new ArrayList<CalendarEvent>(events);
+        Collections.sort(result);
+        return result;
+    }
 
-		if (notifyListeners) {
-			final SelectionChangedEvent event = new SelectionChangedEvent(null);
-			for (final SelectionChangedListener listener : selectionChangedListeners) {
-				listener.selectionChanged(event);
-			}
-		}
-	}
+    @Override
+    public Collection<CalendarEvent> getAllEvents() {
 
-	@Override
-	public Collection<CalendarEvent> getSelectedEvents() {
-		return Collections.unmodifiableSet(new HashSet<CalendarEvent>(selectedEvents));
-	}
+        Collection<CalendarEvent> values = indexedEvents.values();
+        Set<CalendarEvent> result = new HashSet<CalendarEvent>();
 
-	@Override
-	public Collection<CalendarEvent> getEvents(final Date date) {
-		@SuppressWarnings("rawtypes")
-		final Collection events = indexedEvents.getCollection(CalendarUtil.stripTime(date));
-		if (events == null)
-			return new ArrayList<CalendarEvent>();
-		@SuppressWarnings("unchecked")
-		final List<CalendarEvent> result = new ArrayList<CalendarEvent>(events);
-		Collections.sort(result);
-		return result;
-	}
+        for (CalendarEvent event : values) {
+            result.add(event);
+        }
+        return result;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void update(final Observable o, final Object arg) {
-		if (o instanceof CalendarEvent) {
-			final CalendarEvent calendarEvent = (CalendarEvent) o;
-			final Property property = (Property) arg;
-			switch (property) {
-				case START:
-				case END:
+    @Override
+    public List<CalendarEvent> getHolidayEvents(Date date) {
+        Collection<CalendarEvent> events = getEvents(date);
 
-					for (final Object key : new HashSet<Object>(indexedEvents.keySet())) {
-						indexedEvents.remove(key, calendarEvent);
-					}
+        List<CalendarEvent> result = new ArrayList<CalendarEvent>();
+        for(CalendarEvent event : events){
+            if(event.isHoliday()) {
+                result.add(event);
+            }
+        }
 
-					final Collection<Date> dates = CalendarUtil.getDates(calendarEvent.getStart(), calendarEvent.getEnd());
-					for (final Date date : dates) {
-						indexedEvents.put(date, calendarEvent);
-					}
+        return result;
+    }
 
-					notifyObservers(calendarEvent);
+    @SuppressWarnings("unchecked")
+    @Override
+    public void update(final Observable o, final Object arg) {
+        if (o instanceof CalendarEvent) {
+            final CalendarEvent calendarEvent = (CalendarEvent) o;
+            final Property property = (Property) arg;
+            switch (property) {
+                case START:
+                case END:
 
-					final ModelChangedEvent event = new ModelChangedEvent(parent, calendarEvent);
-					for (final ModelChangedListener listener : collectionChangedListeners) {
-						listener.eventChanged(event);
-					}
+                    for (final Object key : new HashSet<Object>(indexedEvents.keySet())) {
+                        indexedEvents.remove(key, calendarEvent);
+                    }
 
-				default:
-					parent.invalidate();
-					parent.repaint();
-					break;
-			}
-		}
-	}
+                    final Collection<Date> dates = CalendarUtil.getDates(calendarEvent.getStart(), calendarEvent.getEnd());
+                    for (final Date date : dates) {
+                        indexedEvents.put(date, calendarEvent);
+                    }
 
-	@Override
-	public void addCollectionChangedListener(final ModelChangedListener listener) {
-		this.collectionChangedListeners.add(listener);
-	}
+                    notifyObservers(calendarEvent);
 
-	@Override
-	public void removeCollectionChangedListener(final ModelChangedListener listener) {
-		this.collectionChangedListeners.remove(listener);
-	}
+                    final ModelChangedEvent event = new ModelChangedEvent(parent, calendarEvent);
+                    for (final ModelChangedListener listener : collectionChangedListeners) {
+                        listener.eventChanged(event);
+                    }
 
-	@Override
-	public void addSelectionChangedListener(final SelectionChangedListener listener) {
-		this.selectionChangedListeners.add(listener);
-	}
+                default:
+                    parent.invalidate();
+                    parent.repaint();
+                    break;
+            }
+        }
+    }
 
-	@Override
-	public void removeSelectionChangedListener(final SelectionChangedListener listener) {
-		this.selectionChangedListeners.remove(listener);
-	}
+    @Override
+    public void addCollectionChangedListener(final ModelChangedListener listener) {
+        this.collectionChangedListeners.add(listener);
+    }
 
-	public int size() {
-		return indexedEvents.size();
-	}
+    @Override
+    public void removeCollectionChangedListener(final ModelChangedListener listener) {
+        this.collectionChangedListeners.remove(listener);
+    }
+
+    @Override
+    public void addSelectionChangedListener(final SelectionChangedListener listener) {
+        this.selectionChangedListeners.add(listener);
+    }
+
+    @Override
+    public void removeSelectionChangedListener(final SelectionChangedListener listener) {
+        this.selectionChangedListeners.remove(listener);
+    }
+
+    public int size() {
+        return indexedEvents.size();
+    }
 }
